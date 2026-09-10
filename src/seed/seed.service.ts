66b89d40a137806e054c2c681cb481329dd9a728
Arturo Urosa <arturo_uo@hotmail.com>
2026-09-10 11:26:22 -0600

    Http adapter

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,19 +1,20 @@
 import { Injectable } from '@nestjs/common';
-import axios, { AxiosInstance } from 'axios';
 import { PokeResponse } from './interfaces/poke-response.interface';
 import { PokemonService } from 'src/pokemon/pokemon.service';
+import { AxiosAdapter } from 'src/common/adapters/axios.adapter';
 
 @Injectable()
 export class SeedService 
 {
-  constructor(private readonly pokemonService: PokemonService) 
+  constructor(
+    private readonly pokemonService: PokemonService,
+    private readonly http: AxiosAdapter)
   {}
-
-  private readonly axios: AxiosInstance = axios;
 
   async populatedDB() 
   {
-    const { data } = await this.axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=1351');
+    //const { data } = await this.axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=1351');
+    const data = await this.http.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=1351');
     // data.results.forEach(async (pokemon) => {
     //   //wait this.axios.get(pokemon.url)
     //   console.log(pokemon.name)
